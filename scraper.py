edd2253d2bcaede2119e31d700e65bf770cced70
Chad Skelton <cskeltonnews@gmail.com>
2015-07-08 16:38:30 -0700

    another test

--- scraper.py
+++ scraper.py
@@ -1,116 +1,33 @@
 #!/usr/bin/env python
 
-# Scraper is setup to search for all Instagram photos with the hashtag #vancouver
-# and then to filter through those to find those that *also* have another hashtag
-# (in the case below, "snow"). Can be altered by simply changing "snow" below to another
-# hashtag. Takes awhile to run because has to go through a lot of #vancouver photos
-# that don't match the other hashtag. Set to 60,000 max so wouldn't hit limits of Excel download
+# import.io seems to be able to scrape Javascript sites like  http://www.yvr.ca/en/flight-information/arriving.aspx
+# can then create a "query" for Excel that creates a simple table; that seems to update at least once a day, maybe live
+# could then pull in that table using the query from scraperwiki on a regular basis (as import.io doesn't have scheduling yet)
 
 import scraperwiki
-import instagram
-from datetime import datetime
+import tweepy
 import time
-import mechanize
-import urllib2
-from instagram.client import InstagramAPI
-import re
+from datetime import datetime
+import smtplib
+import requests
+from BeautifulSoup import BeautifulSoup
+import random
+import datetime
 
-br = mechanize.Browser()
 
-api = InstagramAPI(client_id='895d2b07f8914cf6a50285796b59fd09', client_secret='f0983351e19645a28fd1aef32b87d739')
+url = "https://query.import.io/store/connector/cd943034-faa6-452c-8ddb-94e7bd5113c1/_query?_user=3a3ba656-b1c2-4dc0-9c56-7f0cf7df1f24&_apikey=fOsF7Ia6VLppr77z883Bgfufu8Qks%2B7CE50fxMXHJD%2BFM91XMsXjU3%2BjDoOd0b2hTIHBv8U3KMNsssTqLE0cWw%3D%3D&format=HTML&input/webpage/url=http%3A%2F%2Fwww.yvr.ca%2Fen%2Fflight-information%2Farriving.aspx"
 
-paginationid = 1
+html = requests.get(url)
+htmlpage = html.content
 
-count = 0
+soup = BeautifulSoup(htmlpage)
 
-while paginationid > 0:
+rows = soup.findAll("tr")
 
-    results = api.tag_recent_media(tag_name='vancouver', max_tag_id=paginationid) # uses #vancouver as basic hashtag
-    
-    print results
-    
-    resultsloop = results[0]
-    
-    for m in resultsloop:
-        try:
-            caption = m.caption.text
-            print caption
-        except:
-            print 'no caption'
-            caption = ""
-        if "christmas" in caption or "xmas" in caption: # filters to #vancouver photos also tagged #snow; change "snow" to whatever you want
-            count = count + 1
-            print count
-            print m
-            username = m.user.username
-            fullname = m.user.full_name
-            link = m.link
-            image = m.images['standard_resolution'].url
-            imagelowres = m.images['low_resolution'].url
-            thumbnail = m.images['thumbnail'].url
-            created_time = m.created_time
-            photoid = m.id
-            caption = m.caption.text
-            try:
-                lat = m.location.point.latitude
-                lng = m.location.point.longitude
-            except:
-                lat = "No Lat"
-                lng = "No Long"
-            record={}
-            record["uniqueid"] = ""    
-            record["username"] = ""
-            record["fullname"] = ""
-            record["link"] = ""
-            record["image"] = ""
-            record["created_time"] = ""
-            record["lat"] = ""
-            record["lng"] = ""
-            record["photoid"] = ""
-            record["caption"] = ""
-            record["username"] = username
-            record["fullname"] = fullname
-            record["link"] = link
-            record["image"] = image
-            record["imagelowres"] = imagelowres
-            record["thumbnail"] = thumbnail
-            record["created_time"] = created_time
-            record["lat"] = lat
-            record["lng"] = lng
-            record["caption"] = caption        
-            record["photoid"] = str(photoid)    
-            uniqueid = str(datetime.now())
-            record["uniqueid"] = uniqueid
-            try:
-                scraperwiki.sqlite.save(['uniqueid'], record)
-            except:
-                print 'Could not add record'
-            print record
+for row in rows:
+    print row
+    cells = row.findAll("td")
+    for cell in cells:
+        print cell
         
-        time.sleep(1) # made longer so as not to hit 5000/hr rate limit (not sure if that's per media item or per page of data)
-        
-    url = results[1]
-    
-    print url
-    
-    try:
-        
-        response = br.open(url).read()
-    
-    except:
-        
-        print 'Error loading page in Mechanize'
-        
-    print response
-    
-    try:
-        pagination = re.search('next_max_id\":\"(.+?)\",', response)
-        paginationid = pagination.group(1)
-        print paginationid
-    except:
-        paginationid = 0
-        print 'End of series'
-        
-    if count > 60000:
-        paginationid = 0
 
